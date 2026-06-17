a4ffc1ec720a3c2c21a9f481ce577d2e7f23d7bf
DINARA ASSAN <dinara.assan@gmail.com>
2026-06-17 21:25:49 +0500

    Concert Section 1 a,b,c,d parts done

---
+++ UsrConcerts/Schemas/UsrConcerts_FormPage/UsrConcerts_FormPage.js
@@ -0,0 +1,327 @@
+define("UsrConcerts_FormPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"name": "SaveButton",
+				"values": {
+					"size": "large",
+					"iconPosition": "only-text"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "SideAreaProfileContainer",
+				"values": {
+					"gap": {
+						"columnGap": "large",
+						"rowGap": "none"
+					},
+					"visible": true,
+					"alignItems": "stretch"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "Tabs",
+				"values": {
+					"styleType": "default",
+					"mode": "tab",
+					"bodyBackgroundColor": "primary-contrast-500",
+					"selectedTabTitleColor": "auto",
+					"tabTitleColor": "auto",
+					"underlineSelectedTabColor": "auto",
+					"headerBackgroundColor": "auto",
+					"allowToggleClose": true
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "GeneralInfoTabContainer",
+				"values": {
+					"gap": {
+						"columnGap": "large",
+						"rowGap": "none"
+					},
+					"visible": true,
+					"padding": {
+						"top": "none",
+						"right": "none",
+						"bottom": "none",
+						"left": "none"
+					},
+					"color": "transparent",
+					"borderRadius": "none",
+					"alignItems": "stretch"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "Feed",
+				"values": {
+					"dataSourceName": "PDS",
+					"entitySchemaName": "UsrConcerts"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "AttachmentList",
+				"values": {
+					"columns": [
+						{
+							"id": "2f06d770-6b0e-49a4-85fb-d86a0a5b759c",
+							"code": "AttachmentListDS_Name",
+							"caption": "#ResourceString(AttachmentListDS_Name)#",
+							"dataValueType": 28,
+							"width": 200
+						}
+					]
+				}
+			},
+			{
+				"operation": "insert",
+				"name": "UsrTitle",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 1,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"label": "$Resources.Strings.UsrName",
+					"control": "$UsrName",
+					"labelPosition": "auto",
+					"multiline": false
+				},
+				"parentName": "SideAreaProfileContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "Owner",
+				"values": {
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.PDS_UsrOwner_ee27x9x",
+					"ariaLabel": "",
+					"isAddAllowed": true,
+					"showValueAsLink": true,
+					"labelPosition": "auto",
+					"controlActions": [],
+					"listActions": [],
+					"tooltip": "",
+					"control": "$PDS_UsrOwner_ee27x9x",
+					"valueDetails": null,
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 2,
+						"rowSpan": 1
+					}
+				},
+				"parentName": "SideAreaProfileContainer",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "Code",
+				"values": {
+					"type": "crt.Input",
+					"label": "$Resources.Strings.PDS_UsrCode_oyb3ea3",
+					"control": "$PDS_UsrCode_oyb3ea3",
+					"placeholder": "",
+					"tooltip": "",
+					"readonly": true,
+					"multiline": false,
+					"labelPosition": "auto",
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 3,
+						"rowSpan": 1
+					}
+				},
+				"parentName": "SideAreaProfileContainer",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "ConcertType",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.PDS_UsrConcertType_lgesgud",
+					"ariaLabel": "",
+					"isAddAllowed": true,
+					"showValueAsLink": false,
+					"labelPosition": "auto",
+					"controlActions": [],
+					"listActions": [],
+					"tooltip": "",
+					"control": "$PDS_UsrConcertType_lgesgud",
+					"valueDetails": null
+				},
+				"parentName": "GeneralInfoTabContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "Active",
+				"values": {
+					"type": "crt.Checkbox",
+					"value": true,
+					"disabled": false,
+					"inversed": false,
+					"label": "$Resources.Strings.PDS_UsrActive_4mck5ky",
+					"ariaLabel": "",
+					"labelPosition": "left",
+					"tooltip": "",
+					"control": "$PDS_UsrActive_4mck5ky",
+					"layoutConfig": {
+						"column": 2,
+						"colSpan": 1,
+						"row": 1,
+						"rowSpan": 1
+					}
+				},
+				"parentName": "GeneralInfoTabContainer",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "Notes",
+				"values": {
+					"type": "crt.RichTextEditor",
+					"label": "$Resources.Strings.PDS_UsrNotes_by8a71z",
+					"control": "$PDS_UsrNotes_by8a71z",
+					"labelPosition": "auto",
+					"placeholder": "",
+					"tooltip": "",
+					"needHandleSave": true,
+					"filesStorage": {
+						"masterRecordColumnValue": "$Id",
+						"entitySchemaName": "SysFile",
+						"recordColumnName": "RecordId"
+					}
+				},
+				"parentName": "GeneralInfoTab",
+				"propertyName": "items",
+				"index": 1
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfigDiff: /**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"attributes"
+				],
+				"values": {
+					"UsrName": {
+						"modelConfig": {
+							"path": "PDS.UsrTitle"
+						}
+					},
+					"PDS_UsrCode_oyb3ea3": {
+						"modelConfig": {
+							"path": "PDS.UsrCode"
+						}
+					},
+					"PDS_UsrNotes_by8a71z": {
+						"modelConfig": {
+							"path": "PDS.UsrNotes"
+						}
+					},
+					"PDS_UsrOwner_ee27x9x": {
+						"modelConfig": {
+							"path": "PDS.UsrOwner"
+						}
+					},
+					"PDS_UsrOwner_ee27x9x_List": {
+						"isCollection": true,
+						"modelConfig": {
+							"sortingConfig": {
+								"default": [
+									{
+										"columnName": "Name",
+										"direction": "asc"
+									}
+								]
+							}
+						}
+					},
+					"PDS_UsrConcertType_lgesgud": {
+						"modelConfig": {
+							"path": "PDS.UsrConcertType"
+						}
+					},
+					"PDS_UsrConcertType_lgesgud_List": {
+						"isCollection": true,
+						"modelConfig": {
+							"sortingConfig": {
+								"default": [
+									{
+										"columnName": "Name",
+										"direction": "asc"
+									}
+								]
+							}
+						}
+					},
+					"PDS_UsrActive_4mck5ky": {
+						"modelConfig": {
+							"path": "PDS.UsrActive"
+						}
+					}
+				}
+			},
+			{
+				"operation": "merge",
+				"path": [
+					"attributes",
+					"Id",
+					"modelConfig"
+				],
+				"values": {
+					"path": "PDS.Id"
+				}
+			}
+		]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
+		modelConfigDiff: /**SCHEMA_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [],
+				"values": {
+					"primaryDataSourceName": "PDS"
+				}
+			},
+			{
+				"operation": "merge",
+				"path": [
+					"dataSources"
+				],
+				"values": {
+					"PDS": {
+						"type": "crt.EntityDataSource",
+						"config": {
+							"entitySchemaName": "UsrConcerts"
+						},
+						"scope": "page"
+					}
+				}
+			}
+		]/**SCHEMA_MODEL_CONFIG_DIFF*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
---
+++ UsrConcerts/Schemas/UsrConcerts_ListPage/UsrConcerts_ListPage.js
@@ -0,0 +1,170 @@
+define("UsrConcerts_ListPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"name": "MenuItem_ImportFromExcel",
+				"values": {
+					"clicked": {
+						"request": "crt.ImportDataRequest",
+						"params": {
+							"entitySchemaName": "UsrConcerts"
+						}
+					}
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "FolderTree",
+				"values": {
+					"rootSchemaName": "UsrConcerts"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "DataTable",
+				"values": {
+					"columns": [
+						{
+							"id": "be109c7a-d1ab-e269-e0cd-34c33a844797",
+							"code": "PDS_UsrCode",
+							"caption": "#ResourceString(PDS_UsrCode)#",
+							"dataValueType": 27,
+							"width": 154
+						},
+						{
+							"id": "38c12df9-9610-6785-f223-bf088340aa10",
+							"code": "PDS_UsrTitle",
+							"caption": "#ResourceString(PDS_UsrTitle)#",
+							"dataValueType": 28
+						},
+						{
+							"id": "6f998e90-929e-a545-2283-ebe8b7593cc1",
+							"code": "PDS_UsrConcertType",
+							"caption": "#ResourceString(PDS_UsrConcertType)#",
+							"dataValueType": 10
+						},
+						{
+							"id": "add2245b-6293-3f49-1d40-a0b1a2b963d7",
+							"code": "PDS_UsrOwner",
+							"caption": "#ResourceString(PDS_UsrOwner)#",
+							"dataValueType": 10,
+							"width": 201
+						},
+						{
+							"id": "4522cbb1-08e0-ffdb-8f6d-dc9546aaae25",
+							"code": "PDS_UsrActive",
+							"caption": "#ResourceString(PDS_UsrActive)#",
+							"dataValueType": 12,
+							"width": 131
+						}
+					]
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "Dashboards",
+				"values": {
+					"_designOptions": {
+						"entitySchemaName": "UsrConcerts",
+						"dependencies": [
+							{
+								"attributePath": "Id",
+								"relationPath": "PDS.Id"
+							}
+						],
+						"filters": []
+					}
+				}
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfigDiff: /**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"attributes",
+					"Items",
+					"viewModelConfig",
+					"attributes"
+				],
+				"values": {
+					"PDS_UsrCode": {
+						"modelConfig": {
+							"path": "PDS.UsrCode"
+						}
+					},
+					"PDS_UsrTitle": {
+						"modelConfig": {
+							"path": "PDS.UsrTitle"
+						}
+					},
+					"PDS_UsrConcertType": {
+						"modelConfig": {
+							"path": "PDS.UsrConcertType"
+						}
+					},
+					"PDS_UsrOwner": {
+						"modelConfig": {
+							"path": "PDS.UsrOwner"
+						}
+					},
+					"PDS_UsrActive": {
+						"modelConfig": {
+							"path": "PDS.UsrActive"
+						}
+					}
+				}
+			},
+			{
+				"operation": "merge",
+				"path": [
+					"attributes",
+					"Items",
+					"modelConfig",
+					"sortingConfig"
+				],
+				"values": {
+					"default": [
+						{
+							"direction": "asc",
+							"columnName": "UsrCode"
+						}
+					]
+				}
+			}
+		]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
+		modelConfigDiff: /**SCHEMA_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"dataSources",
+					"PDS",
+					"config"
+				],
+				"values": {
+					"entitySchemaName": "UsrConcerts",
+					"attributes": {
+						"UsrCode": {
+							"path": "UsrCode"
+						},
+						"UsrTitle": {
+							"path": "UsrTitle"
+						},
+						"UsrConcertType": {
+							"path": "UsrConcertType"
+						},
+						"UsrOwner": {
+							"path": "UsrOwner"
+						},
+						"UsrActive": {
+							"path": "UsrActive"
+						}
+					}
+				}
+			}
+		]/**SCHEMA_MODEL_CONFIG_DIFF*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
